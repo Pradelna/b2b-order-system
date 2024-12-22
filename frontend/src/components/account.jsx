eb91c675c6ef7d2598a07b6a8c7e9dbe13bc5adf
Sergey Sokov <sokov.tlt@gmail.com>
2024-12-22 15:20:40 +0100

    added account page

--- frontend/src/components/account.jsx
+++ frontend/src/components/account.jsx
@@ -8,55 +8,104 @@ function Account({ language, languageData }) {
   }
 
   return (
-    <section id="services" className="services">
-        <div className="container">
-            <div className="services__wrap">
+        <div className="container margin-top-200">
+            <div className="row">
+              <div className="col-8">
+                <div id="company-top" className="row">
+                  
+                  <div className="col-6">
+                    <div className="card dashboard">
+                      <div className="card-body">
+                        <h5 className="card-title">Company name</h5>
+                        <h6 className="card-subtitle mb-2 text-body-secondary">ICO 44502693</h6>
+                        <p className="card-text">+420 944 764 873</p>
+                        <p className="card-text">hotel@gmail.com</p>
+                        <p className="card-text">Petr Kucer</p>
+                      </div>
+                    </div>
+                  </div>
+                  
+                  <div className="col-3">
+                    <div className="card dashboard-button">
+                      <div className="card-body">
+                        <h5 className="card-title">Orders history</h5>
+                        <h6 className="card-subtitle mb-2 text-body-secondary">View order history</h6>
+                        <p className="card-text">Some text</p>
+                      </div>
+                    </div>
+                  </div>
 
-                <h2>{data.title}</h2>
+                  <div className="col-3">
+                    
+                    <div className="card dashboard-button mini new-order">
+                      <div className="card-body">
+                        <h5 className="card-title">new order</h5>
+                      </div>
+                    </div>
+                    
+                    <div className="card dashboard-button mini">
+                      <div className="card-body">
+                        <h5 className="card-title">new place</h5>
+                      </div>
+                    </div>
+                    
+                  </div>
+
+                </div>
                 
-             <div className="services__item">
-                    <img src="/wp-content/uploads/2020/09/services1.png" alt=""/>
-                    <div className="services__item__text">
-                        <h3>{data.sub_title_1}</h3>
-                        <div dangerouslySetInnerHTML={{ __html: data.description_1 }} />
+          
+                <div className="row mt-5">
+                  
+                  <div className="col-12">
+                    
+                    <div className="card dashboard place-card">
+                      <div className="card-body place">
+                        <img src="src/assets/dom.webp" alt="" />
+                        <h5 className="card-title">Hotel Praha</h5>
+                        <p className="card-text">Sokolovska 15, 831 43</p>
+                        <button className="call dash-button">new order</button>
+                      </div>
                     </div>
-                </div>
+                    
+                  </div>
+                  
+                  <div className="col-12">
+                    
+                    <div className="card dashboard place-card">
+                      <div className="card-body place">
+                        <img src="src/assets/dom.webp" alt="" />
+                        <h5 className="card-title">Hotel Praha</h5>
+                        <p className="card-text">Sokolovska 15, 831 43</p>
+                        <button className="call dash-button">new order</button>
+                      </div>
+                    </div>
+                    
+                  </div>
+                  
+                  <div className="col-12">
+                    
+                    <div className="card dashboard place-card">
+                      <div className="card-body place">
+                        <img src="src/assets/dom.webp" alt="" />
+                        <h5 className="card-title">Hotel Praha</h5>
+                        <p className="card-text">Sokolovska 15, 831 43</p>
+                        <button className="call dash-button">new order</button>
+                      </div>
+                    </div>
+                    
+                  </div>
 
-             <div className="services__item">
-                    <img src="/wp-content/uploads/2020/09/services2.png" alt=""/>
-                    <div className="services__item__text">
-                      <h3>{data.sub_title_2}</h3>
-                      <div dangerouslySetInnerHTML={{ __html: data.description_2 }} />
-                    </div>
                 </div>
+                
+          
+          
 
-             <div className="services__item">
-                    <img src = "/wp-content/uploads/2020/09/services3.png" alt=""/>
-                    <div className="services__item__text">
-                      <h3>{data.sub_title_3}</h3>
-                      <div dangerouslySetInnerHTML={{ __html: data.description_3 }} />
-                    </div>
-                </div>
 
-             <div className="services__item">
-                    <img src="/wp-content/uploads/2020/09/services4.png" alt=""/>
-                    <div className="services__item__text">
-                      <h3>{data.sub_title_4}</h3>
-                      <div dangerouslySetInnerHTML={{ __html: data.description_4 }} />
-                    </div>
-                </div>
-
-             <div className="services__item">
-                    <img src="/wp-content/uploads/2020/09/services5.png" alt=""/>
-                    <div className="services__item__text">
-                      <h3>{data.sub_title_4}</h3>
-                      <div dangerouslySetInnerHTML={{ __html: data.description_4 }} />
-                    </div>
-                </div>
+              </div>
 
             </div>
+
         </div>
-    </section>
   );
 }
 
