cb034bc65b6cf48ebcff10113e88d4318731f086
Sergey Sokov <sokov.tlt@gmail.com>
2025-03-16 11:04:40 +0100

    added reset email and fix some bugs

--- frontend/src/components/MainPageWithPrefix.tsx
+++ frontend/src/components/MainPageWithPrefix.tsx
@@ -3,17 +3,34 @@ import { useParams, useNavigate } from "react-router-dom";
 import { LanguageContext } from "../context/LanguageContext";
 import Header from "./Header";
 import StartBanner from "./landing/StartBanner";
-import About from "./landing/About.js";
-import Services from "./landing/Services.js";
-import Technology from "./landing/Technologies.js";
-import Price from "./landing/Price.js";
-import Contacts from "./landing/Contacts.js";
+import About from "./landing/About";
+import Services from "./landing/Services";
+import Technology from "./landing/Technologies";
+import Price from "./landing/Price";
+import Contacts from "./landing/Contacts";
 import Footer from "./Footer";
+import { Helmet } from "react-helmet";
 
 const MainPageWithPrefix: React.FC = () => {
-    const { language, handleLanguageChange } = useContext(LanguageContext);
+    const { currentData, language, handleLanguageChange } = useContext(LanguageContext);
     const { lang } = useParams<{ lang: string }>();
     const navigate = useNavigate();
+
+    let title, description, htmlLang;
+    if (currentData?.lang === "en") {
+        title = "Professional laundry in Prague, washing and dry cleaning";
+        description = "Washing and cleaning work uniforms, cleaning feather products, removing stains, basic processing of linen using dry cleaning machines";
+        htmlLang = "en-GB";
+    } else if (currentData?.lang === "ru") {
+        title = "Профессиональная прачечная в Праге, стирка и химчистка";
+        description = "Стирка и чистка рабочей формы, спец одежды, чистка перьевых изделий, удаление пятен, основная обработка белья с использованием машин химчистки";
+        htmlLang = "ru-RU";
+    } else {
+        // Язык по умолчанию
+        title = "Profesionální prádelna v Praze, praní a chemické čištění";
+        description = "Praní a čištění uniforem, oděvů, čištění péřových výrobků, odstranění skvrn, základní ošetření prádla za pomocí strojů na chemické čištění.";
+        htmlLang = "cs-CZ";
+    }
 
     useEffect(() => {
         if (lang === "cz") {
@@ -25,6 +42,11 @@ const MainPageWithPrefix: React.FC = () => {
 
     return (
         <>
+            <Helmet>
+                <html lang={htmlLang}></html>
+                <title>{title}</title>
+                <meta name="description" content={description} />
+            </Helmet>
             <Header />
             <StartBanner />
             <About />
