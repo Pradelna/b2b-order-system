59fd6715abebd13e06434073edf492fb29533494
Sergey Sokov <sokov.tlt@gmail.com>
2025-04-03 14:37:09 +0200

    settings customer apps

--- washpr/customer/signals.py
+++ washpr/customer/signals.py
@@ -1,10 +1,10 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
+from django.conf import settings
 from .models import Customer
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_customer_for_user(sender, instance, created, **kwargs):
     print("create_customer_for_user")
     if created:
