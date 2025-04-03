a7a43dda03b7d3a60b92290d9f68b3b57647ee4a
Sergey Sokov <sokov.tlt@gmail.com>
2025-04-03 14:20:12 +0200

    settings customer apps

--- washpr/washpr/settings.py
+++ washpr/washpr/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'landing',
-    'customer',
+    'customer.apps.CustomerConfig',
     'order',
     'place',
     'rest_framework',
