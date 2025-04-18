1ee1aebbe744aa1fc2ce056321bee6cf5610c8f3
root <root@vm4027643.stark-industries.solutions>
2025-04-18 17:41:30 +0200

    python manage.py migrate django_celery_beat

--- washpr/entrypoint.sh
+++ washpr/entrypoint.sh
@@ -11,6 +11,7 @@ python manage.py makemigrations
 
 echo "Apply database migrations"
 python manage.py migrate --noinput
+python manage.py migrate django_celery_beat
 
 echo "Install default data"
 python manage.py loaddata landing.json
