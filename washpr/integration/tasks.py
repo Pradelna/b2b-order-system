aee6082e3b9e7d60184495f1c02e1ffaeea859b8
Sergey Sokov <sokov.tlt@gmail.com>
2025-04-21 15:08:23 +0200

    fix send order removed time threshold

--- washpr/integration/tasks.py
+++ washpr/integration/tasks.py
@@ -381,8 +381,8 @@ def send_orders_task():
     from order.models import Order  # Импортируем модель заказа из приложения order
 
     # Выбираем заказы, не отправленные ранее и созданные более 35 минут назад
-    time_threshold = timezone.now() - timedelta(minutes=35)
-    orders = Order.objects.filter(active=False, canceled=False, processed=True ,created_at__lte=time_threshold)
+    # time_threshold = timezone.now() - timedelta(minutes=35)
+    orders = Order.objects.filter(active=False, canceled=False, processed=True)
 
     results = []
     fail_results = []
@@ -442,7 +442,7 @@ def create_orders_task():
     from order.models import Order  # Импортируем модель заказа из приложения order
 
     # Выбираем заказы, не отправленные ранее и созданные более 35 минут назад
-    time_threshold = timezone.now() - timedelta(minutes=1)
+    time_threshold = timezone.now() - timedelta(minutes=35)
     orders = Order.objects.filter(
         processed=False,
         canceled=False,
