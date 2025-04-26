ec70c33e807592835d01ca540f0c2ddeeb85f9e6
root <root@vm4027643.stark-industries.solutions>
2025-04-26 15:44:59 +0200

    order history

--- frontend/src/components/order/OrderHistory.tsx
+++ frontend/src/components/order/OrderHistory.tsx
@@ -217,17 +217,13 @@ const OrderHistory: React.FC<OrderHistoryProps> = ({ placeId, orders = [], setOr
                                         key={order.id}
                                         className={`card ${expandedOrders[order.id] ? "expanded" : ""}`}
                                         onClick={() => toggleExpand(order.id)}
-                                        // style={{ display: (order.rp_status === 0 && order.every_week) || (
-                                        //     order.id === order.group_pair_id &&
-                                        //         order.rp_status !== 20 &&
-                                        //         order.rp_status !== 10) ? "none" : "block",
-                                        //     '--card-height': dynamicHeight,} as React.CSSProperties}
-					style={{ display: (order.rp_status === 0 && order.every_week) ||
-                                        (order.id === order.group_pair_id && order.type_ship !== "pickup_ship_one") ? "none" : "block",
+                                        style={{ display: (order.rp_status === 0 && order.every_week) || (
+                                            order.id === order.group_pair_id &&
+                                                order.rp_status !== 20 &&
+                                                order.rp_status !== 10) ? "none" : "block",
                                             '--card-height': dynamicHeight,} as React.CSSProperties}
                                     >
-                                        {order.type_ship === "pickup_ship_one" ||
-                                            (order.type_ship !== "pickup_ship_one" && order.id !== order.group_pair_id) ? (<>
+                                        {(order.id !== order.group_pair_id || (order.rp_status === 20 || order.rp_status === 10)) ? (<>
                                         <div className="history-icon">
                                             <FontAwesomeIcon icon={faTruck} />
                                         </div>
