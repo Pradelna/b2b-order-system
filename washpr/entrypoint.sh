d4408c9f7927c77a7711f49ea99f754d261e79ef
Sergey Sokov <sokov.tlt@gmail.com>
2025-04-26 15:57:15 +0200

    fixed migrations

--- washpr/entrypoint.sh
+++ washpr/entrypoint.sh
@@ -2,19 +2,26 @@
 
 echo "Apply database make migrations"
 python manage.py makemigrations accounts
-python manage.py migrate
+python manage.py migrate accounts
 python manage.py makemigrations customer
+python manage.py migrate customer
 python manage.py makemigrations place
+python manage.py migrate place
 python manage.py makemigrations order
+python manage.py migrate order
 python manage.py makemigrations landing
-python manage.py makemigrations
+python manage.py migrate landing
 
 echo "Apply database migrations"
 python manage.py migrate --noinput
 python manage.py migrate django_celery_beat
 
 echo "Install default data"
-python manage.py loaddata landing.json
+if python manage.py loaddata landing.json; then
+  echo "Default data loaded successfully."
+else
+  echo "Warning: Failed to load landing.json. Continuing without aborting."
+fi
 
 echo "Collect static files"
 python manage.py collectstatic --noinput
