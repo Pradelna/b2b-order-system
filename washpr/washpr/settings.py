c6825ab5f60d1ad536b48d570b717bf827c858ca
Sergey Sokov <sokov.tlt@gmail.com>
2025-05-19 16:11:35 +0200

    Test VPS fix settings Version 0.4.6

--- washpr/washpr/settings.py
+++ washpr/washpr/settings.py
@@ -111,8 +111,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', 'washpr'),
-        'USER': os.getenv('POSTGRES_USER', ''),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.getenv('POSTGRES_HOST', 'postgres'),
         'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
