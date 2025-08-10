c97cdcafad03e590b22fa671a616181dfaba5d9e
Sergey Sokov <sokov.tlt@gmail.com>
2025-08-10 12:01:48 +0200

    change entrypoints checking if migrations

---
+++ check_minio.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+set -e
+
+# Загружаем переменные из .env (если есть)
+if [ -f .env ]; then
+  export $(grep -v '^#' .env | xargs)
+else
+  echo "❌ Файл .env не найден!"
+  exit 1
+fi
+
+# Конфигурация из переменных окружения
+ALIAS_NAME=myminio
+MINIO_URL=${AWS_S3_ENDPOINT_URL:-http://minio:9000}
+ROOT_USER=$MINIO_ROOT_USER
+ROOT_PASS="$MINIO_ROOT_PASSWORD"
+NEW_USER=$AWS_ACCESS_KEY_ID
+NEW_PASS="$AWS_SECRET_ACCESS_KEY"
+BUCKET_NAME=$AWS_STORAGE_BUCKET_NAME
+
+# Настроить alias (если не настроен)
+mc alias set $ALIAS_NAME $MINIO_URL "$ROOT_USER" "$ROOT_PASS"
+
+# Проверка и создание пользователя
+if mc admin user list $ALIAS_NAME | grep -q "$NEW_USER"; then
+    echo "✅ Пользователь $NEW_USER уже существует."
+else
+    echo "➕ Создаём пользователя $NEW_USER..."
+    mc admin user add $ALIAS_NAME "$NEW_USER" "$NEW_PASS"
+    mc admin policy attach $ALIAS_NAME readwrite --user "$NEW_USER"
+    echo "✅ Пользователь $NEW_USER создан и получил политику readwrite."
+fi
+
+# Проверка и создание бакета
+if mc ls $ALIAS_NAME | grep -q "$BUCKET_NAME/"; then
+    echo "✅ Бакет $BUCKET_NAME уже существует."
+else
+    echo "➕ Создаём бакет $BUCKET_NAME..."
+    mc mb $ALIAS_NAME/$BUCKET_NAME
+    echo "✅ Бакет $BUCKET_NAME создан."
+fi
--- washpr/entrypoint.sh
+++ washpr/entrypoint.sh
@@ -1,17 +1,33 @@
 #!/bin/sh
+set -e
 
-echo "Apply database make migrations"
-python manage.py makemigrations accounts
-python manage.py migrate
-python manage.py makemigrations customer
-python manage.py makemigrations place
-python manage.py makemigrations order
-python manage.py makemigrations landing
-python manage.py makemigrations
+echo "Checking for model changes..."
 
-echo "Apply database migrations"
-python manage.py migrate --noinput
-python manage.py migrate django_celery_beat
+if ! python manage.py makemigrations --dry-run --check; then
+  echo "Found changes, making migrations..."
+
+  echo "Apply database make migrations"
+  python manage.py makemigrations accounts
+  python manage.py migrate
+  python manage.py makemigrations customer
+  python manage.py makemigrations place
+  python manage.py makemigrations order
+  python manage.py makemigrations landing
+  python manage.py makemigrations
+
+else
+  echo "No changes detected, skipping makemigrations."
+fi
+
+echo "Checking for unapplied migrations..."
+
+if python manage.py showmigrations --plan | grep '\[ \]'; then
+  echo "Applying migrations..."
+  python manage.py migrate --noinput
+  python manage.py migrate django_celery_beat
+else
+  echo "No unapplied migrations, skipping migrate."
+fi
 
 echo "Install default data"
 python manage.py loaddata landing.json
